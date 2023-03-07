31c33d1d228fdaab98a25b52f7dcddd5fa6d2ee8
andrada08 <86690938+andrada08@users.noreply.github.com>
2023-03-07 16:32:44 +0000

    new_tests

--- Communication/test_tcp.m
+++ Communication/test_tcp.m
@@ -1,17 +1,52 @@
-server = tcpserver("0.0.0.0",30000)
+server = tcpserver("163.1.249.17",30000)
 
 data = read(server,server.NumBytesAvailable,"double");
 plot(data);
 
-write(server, data, "double")
+data = double([1:64]);
+write(server, data)
+write(server,'hello world','char')
+write(server,"hello world",'string')
 
-write(server,"hello world","string")
+string = 'hello world';
+string = [string 0 0 0 0];
+string = string(1:end-mod(length(string),4));
+write(server,string,'string');
 
 read(server,server.NumBytesAvailable, "double");
 
 read(server,server.NumBytesAvailable, "string")
 
-% configureTerminator(server,"CR",10)
+configureTerminator(server,"LF")
+
+%% UDP check
+
+u = udpport("IPV4");
+
+write(u,'/i0001',"uint8","163.1.249.37",30000);
+
+string = 'hello world';
+string = [string 0 0 0 0];
+string = string(1:end-mod(length(string),4));
+
+write(u,string,"string","163.1.249.37",30000);
+
+string = ',';
+string = [string 0 0 0 0];
+string = string(1:end-mod(length(string),4));
+
+
+write(u,'/start  ,f  B4  ',"163.1.249.37",30000);
+
+
+
+
+bonsai_oscsend(u,'/start',"163.1.249.37",30000,'i',45);
+
+data2 = ['/start' 0 0 ',i' 0 0 0 0 0 '-'];
+write(u,data2,"163.1.249.37",30000);
+
+
 
 %% check if you can do two servers in one matlab
 server2 = tcpserver("0.0.0.0",20000)
