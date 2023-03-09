7890618f6abeca3822645943c31e4d32474cad74
andrada08 <86690938+andrada08@users.noreply.github.com>
2023-03-09 18:25:26 +0000

    update com from mc comp

--- Communication/mc_start_stop_experiment.m
+++ Communication/mc_start_stop_experiment.m
@@ -1,14 +1,14 @@
 protocol = 'trial_test';
 name = 'MissMouse0801';
 
-server = tcpserver("0.0.0.0",10000);
+client = tcpclient("163.1.249.37",50001);
 
 s = struct('mouse', name, 'protocol', protocol);
 encode_s = jsonencode(s);
-write(server,encode_s,'string');
+write(client,encode_s,'string');
 
 %% send start message
-write(server,'start','string');
+write(client,'start','string');
 
 %% send stop message
-write(server,'stop','string');
+write(client,'stop','string');
