d4e139857bfb84ea4ff30166492ee7025195ff9c
andrada08 <86690938+andrada08@users.noreply.github.com>
2023-03-30 11:34:59 +0100

    add valve to arduino leonardo

--- Arduino stuff/arduino_rotary_encoder/arduino_rotary_encoder.ino
+++ Arduino stuff/arduino_rotary_encoder/arduino_rotary_encoder.ino
@@ -7,14 +7,28 @@
 
 #define encoder0PinA 3        // sensor A of rotary encoder
 #define encoder0PinB 7        // sensor B of rotary encoder
+#define SValvePin 1           // digital pin controlling the solenoid valve
+
 
 // variables for rotary encoder
 volatile signed int encoder0Pos = 0;    // variable for counting ticks of rotary encoder
+
+// variables for pintch valve of reward system
+const byte numChars = 6;
+char receivedChars[numChars];   // an array to store the received data
+boolean newData = false;
+int TimeON = 0;             // new for this version
+int TempVar = 0;
+boolean TimerFinished = false;
+uint32_t StartTime = 0;      // variable to store temporary timestamps of previous iteration of the while loop
+
 
 void setup() {
 
   pinMode(encoder0PinA, INPUT);   // rotary encoder sensor A
   pinMode(encoder0PinB, INPUT);   // rotary encoder sensor B
+
+  pinMode(SValvePin, OUTPUT);     // solenoid valve
 
   // interrupts for rotary encoder
   attachInterrupt(digitalPinToInterrupt(encoder0PinA), doEncoderA, FALLING);
@@ -26,6 +40,9 @@ void setup() {
 }
 
 void loop() {
+
+  GetBonsaiInput();
+  ActivatePV();
 
 }
 
@@ -41,5 +58,57 @@ void doEncoderA() {
     Serial.print("\n");
 }
 
+
+/////////////////////////////////////////////////////////////////////////////
+// Read inputs from Bonsai. It is an integer specifing the amount of time in ms the pintch valve stays open
+void GetBonsaiInput() { // part of code taken from http://forum.arduino.cc/index.php?topic=396450.0
+  static byte ndx = 0;
+  char endMarker = '\r';
+  char rc;
+
+  if (Serial.available() > 0) {
+    rc = Serial.read();
+
+    if (rc != endMarker) {
+      receivedChars[ndx] = rc;
+      ndx++;
+      if (ndx >= numChars) {
+        ndx = numChars - 1;
+      }
+    }
+    else {
+      receivedChars[ndx] = '\0'; // terminate the string
+      ndx = 0;
+      newData = true;
+    }
+  }
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// Timer for the output signal to the pintch valve to stay high
+void ActivatePV() {
+  if (newData == true) {
+    TimeON = 0;             // zero previous time
+    TimeON = atoi(receivedChars);   // convert array of chars to integers
+    if (TempVar == 0) {
+      StartTime = millis();
+      TempVar = 1;
+    }
+    newData = false;
+  }
+  if (TempVar == 1) {
+    // start checking the time and keep the valve open as long as you wish irrespective of what happens to Serial.read()
+    if ((millis() - StartTime) <= (uint32_t)TimeON) {
+      digitalWrite(SValvePin, HIGH); // open valve
+      TimerFinished = false;
+      TempVar = 1;
+    }
+    else {
+      digitalWrite(SValvePin, LOW); // close valve
+      TimerFinished = true;
+      TempVar = 0;
+    }
+  }
+}
 
 // EOF
